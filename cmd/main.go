e3e5d7adfe9c6e516db09792a6cc6586a62499a8
Буянов Всеволод <mors@nemors.ru>
2024-02-09 15:32:25 +0300

    added struct server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
-package cmd
+package main
+
+import (
+	"github.com/vpbuyanov/gw-backend-go/internal/server"
+
+	"github.com/vpbuyanov/gw-backend-go/configs"
+)
+
+func main() {
+	config := configs.LoadConfig()
+	runner := server.GetServer(config)
+
+	err := runner.Start()
+	if err != nil {
+		return
+	}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,29 @@
 package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+
+	"github.com/vpbuyanov/gw-backend-go/configs"
+)
+
+type server struct {
+	config configs.Config
+}
+
+type Server interface {
+	Start() error
+}
+
+func GetServer(config configs.Config) Server {
+	return &server{config: config}
+}
+
+func (s *server) Start() error {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	_ = app.Group("/api")
+
+	return app.Listen(s.config.Server.Port)
+}
