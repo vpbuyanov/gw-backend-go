fbe2f3b79736576af66ccd2a5ef6d01dfd5aa1a0
Vsevolod Buyanov <mors@nemors.ru>
2024-02-14 17:06:15 +0300

    docker && cicd  (#3)
    * 1.21.6 -> 1.21.7 && change docker-compose
    
    * fix
    
    * added ci cd push docker hub
    
    * fixed
    
    * fixed
    
    * update
    
    * fixed

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.21.6-alpine AS builder
+FROM golang:1.21-alpine AS builder
 
 WORKDIR /usr/local/src
 
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
+
 	"github.com/vpbuyanov/gw-backend-go/configs"
+	"github.com/vpbuyanov/gw-backend-go/internal/databases/postgres"
+	"github.com/vpbuyanov/gw-backend-go/internal/databases/redis"
 	"github.com/vpbuyanov/gw-backend-go/internal/server"
 )
 
@@ -9,7 +13,24 @@ func main() {
 	config := configs.LoadConfig()
 	runner := server.GetServer(config)
 
-	err := runner.Start()
+	pg := postgres.NewReposPostgresql(config.Postgres)
+	dbRedis := redis.NewReposRedis(config.Redis)
+
+	err := pg.Connect()
+	if err != nil {
+		logrus.WithError(err).Println("can't connect to pg")
+	}
+
+	logrus.Println("pg connect")
+
+	err = dbRedis.Connect()
+	if err != nil {
+		logrus.WithError(err).Println("can't connect to redis")
+	}
+
+	logrus.Println("redis connect")
+
+	err = runner.Start()
 	if err != nil {
 		return
 	}
