1f54dd1139295013aa16e4f30cfdfdce5d7e81aa
Буянов Всеволод <mors@nemors.ru>
2024-04-23 01:09:55 +0300

    fix

--- migrations/000001_forum.up.sql
+++ migrations/000001_forum.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "user" (
       id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
       name VARCHAR(50) NOT NULL,
       email VARCHAR(100) NOT NULL,
-      hash_pass VARCHAR(250) NOT NULL,
+      hash_pass VARCHAR(1000) NOT NULL,
       is_admin BOOLEAN DEFAULT FALSE
 );
 
