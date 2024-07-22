6d156a64a9137b50342c1c364b41275fe4afe682
Буянов Всеволод <mors@nemors.ru>
2024-07-22 23:46:33 +0300

    update ci

--- internal/handlers/user/entity.go
+++ internal/handlers/user/entity.go
@@ -6,9 +6,9 @@ type loginUserRequest struct {
 }
 
 type registrationUserRequest struct {
-	Name     string `json:"name" binding:"required"`
-	Surname  string `json:"surname"  binding:"required"`
-	Email    string `json:"email"  binding:"required"`
-	Phone    string `json:"phone"  binding:"required"`
-	Password string `json:"password"  binding:"required"`
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	Email    string `json:"email"`
+	Phone    string `json:"phone"`
+	Password string `json:"password"`
 }
